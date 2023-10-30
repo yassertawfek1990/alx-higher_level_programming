25a502c636d960bf0860abf6ec3626aa52f19a74
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-10-30 16:52:31 -0700

    c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - d
+ * @list: linke
+ * Return: 1
+ */
+int check_cycle(listint_t *list)
+{
+        listint_t *s = list;
+        listint_t *f = list;
+
+	while(f && f->next)
+	{
+		s = s->next;
+		f = f->next->next;
+		if (s == f)
+			return (1);
+	)
+	return (0);
+}
