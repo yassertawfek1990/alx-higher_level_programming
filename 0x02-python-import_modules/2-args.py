4653bbc95d9a6464fef4bcab7a1b0e90b017fbee
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-02 12:43:01 -0700

    c

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
 
     t = len(sys.argv) - 1
-    if t == 1:
-        print("1 argument:")
-    elif t == 0:
+    if t == 0:
         print("0 arguments.")
+    elif t == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(t))
     for i in range (t):
