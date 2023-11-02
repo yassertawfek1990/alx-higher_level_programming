f130f8fdf7a0677fef7d42c1dd7d03e895ea7820
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-02 12:41:20 -0700

    c

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(t))
     for i in range (t):
-        print("{}: {}".format(i + 1, t[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
