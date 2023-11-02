c0d066fa554d1a4f0335f2eb6ceb13da7a4cd3b5
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-02 14:29:40 -0700

    c

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     if sys.argv[2] not in [+, -, *, /]:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
     a = int(sys.argv[1])
     op = (sys.argv[2]
     b = int(sys.argv[3])
