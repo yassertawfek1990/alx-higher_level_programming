bc0e1bdcd07dc644df2be3b3326aea47230b6d1f
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-05 13:21:32 -0800

    c

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in range(len(matrix)):
+        for c in range(len(matrix[x])):
+            if c != 0:
+                print(" ", end = '')
+            print("{:d}".format(matrix[x][c]), end = '')
+        print()
