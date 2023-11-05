baaec6a909432997c92856162954374566a40d7a
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-05 14:02:33 -0800

    c

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,10 +11,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) < 1:
         b1 = 0
     else:
-        b1 = tuple_a[1]
+        b1 = tuple_b[0]
     if len(tuple_b) < 2:
         b2 = 0
     else:
-        b2 = tuple_a[1]
+        b2 = tuple_b[1]
  
     return (a1 + b1, a2 + b2)
