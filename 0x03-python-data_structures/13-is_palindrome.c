f229b586ee8bc9609538f09167b2b8ac45690ce9
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-06 08:01:01 -0800

    c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * pan - checks if a sin
+ * @head: pointer to 
+ * Return: 0 if it is not
+ */
+int pan(listint_t head, listint_t *head)
+{
+	if (end == NULL)
+		return (1);
+	if (pan(head, end->next) && (*head->n == end->n))
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+}
+
+/**
+ * is_palindrome - checks if a s
+ * @head: pointer t
+ * Return: 0 if it is not a palind
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (pan(head, *head);
+}
