8b847b1bc8ac91abcf82dff9a5bf0941ade134e7
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-08 15:03:17 -0800

    c

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    t = 1
+    c = 0
+    w = 0
+    for x in range(len(my_list)):
+        t = 1
+        for i in range(len(my_list[x])):
+            t *= my_list[x][i]
+        w += my_list[x][1]
+        c += t
+    return (c / w)
