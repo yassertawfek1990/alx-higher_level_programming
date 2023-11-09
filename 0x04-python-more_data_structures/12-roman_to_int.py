50d06df211deb57e071206b75cf4035d225cb568
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-08 16:09:43 -0800

    c

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,16 +4,11 @@ def roman_to_int(roman_string):
         return 0
     d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     t = 0
-    for i in range(len(roman_string)):
-        if roman_string[i] in d.keys():
-            if i > 0:
-                if d[roman_string[i]] >= d[roman_string[i - 1]]:
-                    t -= d[roman_string[i]]
-                    t *= -1
-                else:
-                    t += d[roman_string[i]]
-            else:
-                t += d[roman_string[i]]
+    n = 0
+    for i in reversed(roman_string):
+        n = d[i]
+        if t < n * 5:
+            t +=n
         else:
-            t = 0
+            t -= n
     return (t)
