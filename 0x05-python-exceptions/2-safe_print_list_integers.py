01f30afad63fa81ec805e3b3f14c85ced680c29b
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-20 14:26:53 -0800

    c

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    t = 0
+    m = 0
+    while m < x:
+        try:
+            print("{:d}".format(my_list[m]), end='')
+            t += 1
+        except (TypeError,ValueError):
+            pass
+        m += 1
+    print()
+    return t
