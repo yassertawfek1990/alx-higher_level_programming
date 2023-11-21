d95e8d7c9732b221b8b88a09bb77da450cd752a9
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-21 10:05:21 -0800

    c

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,8 @@ class Square:
     """Represent a square."""
     def __init__(self, size=0):
         """Constructor
-        a
+        Args:
+            kdwk
         """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
