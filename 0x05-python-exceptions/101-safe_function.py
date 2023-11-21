41bc38f0d5a555fa2cbc36dab1db3e1fa40250e6
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-20 16:38:57 -0800

    c

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        r = fct(args)
+        return (r)
+    except Exception as x:
+        print("Exception: ", x, file=sys.stderr)
+        return (None)
