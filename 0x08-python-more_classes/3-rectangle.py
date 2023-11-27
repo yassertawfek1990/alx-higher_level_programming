c5c08054043fb299b79d6f809a9a60cf35640a3b
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-27 11:04:53 -0800

    c

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,13 +52,15 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
      def __str__(self):
-        """Return the p character."""
+        """Return the p 
+        character.
+        """
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rect = []
+        r = []
         for b in range(self.__height):
-            [rect.append('#') for k in range(self.__width)]
+            [r.append('#') for k in range(self.__width)]
             if b != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+                r.append("\n")
+        return ("".join(r))
