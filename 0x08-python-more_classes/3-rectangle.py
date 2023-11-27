b47a8057b61000a872dc9028c4712256b4d7963f
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-27 11:06:45 -0800

    c

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
-     def __str__(self):
+    def __str__(self):
         """Return the p 
         character.
         """
