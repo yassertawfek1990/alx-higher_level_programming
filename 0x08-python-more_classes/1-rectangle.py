ceef19d4968754ab83c1e33d552c01e932b92b04
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-11-27 10:12:05 -0800

    c

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
             width (int): rectangle.
             height (int): The heigh rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
     @property
     def width(self):
         """Get/srectangle."""
