cf4e37ce13110f10644d94bf07ed656c8512b3ab
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-04 12:42:38 -0800

    c

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define ska"""
+
+
+class BaseGeometry:
+    """ddaddsc f"""
+    def area(self):
+        """"dd"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """ jasdjajdc sdsd"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value =< 0:
+            raise ValueError("{} must be greater than 0".format(name))
