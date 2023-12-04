2417893a2dbacd8e02c0df04f6acb113f56ad944
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-04 11:37:27 -0800

    c

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines s"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return aaaa"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
