904fccc06059ed1ba5894051da6cc9a4c020cba6
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-04 11:44:08 -0800

    c

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,4 @@
 def inherits_from(obj, a_class):
     """Return skad"""
     def inherits_from(obj, a_class):
-        if isinstance(obj, a_class) and type(obj) != a_class:
-            return True
-        else:
-            return False
+        return isinstance(obj, a_class) and type(obj) != a_class
