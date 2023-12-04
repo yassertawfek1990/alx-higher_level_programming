5a1f35f851c3d4bfb51915792a2bf5c482490927
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-04 13:27:13 -0800

    c

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define las ald"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+    def __init__(self, size):
+        """self"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(size):
+        """gsgc dgrg"""
+        return self.__size ** 2
+
