6c8003bf01ab17d89c99f455255357b3e3db30f8
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-05 15:36:10 -0800

    c

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,9 +6,9 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        it = load_from_json_file("add_item.json")
-    except Exception:
-        it = []
-    it.extend(sys.argv[1:])
-    save_to_json_file(it, "add_item.json")
+try:
+    it = load_from_json_file("add_item.json")
+except Exception:
+    it = []
+it.extend(sys.argv[1:])
+save_to_json_file(it, "add_item.json")
