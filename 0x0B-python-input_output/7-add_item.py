f2d2294a62ff1ecbdc76f06bd8ca67c4f411685f
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-05 15:32:14 -0800

    c

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""define dd"""
-
-
+"""define ffsf"""
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-l = list(sys.argv[1:])
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    n = load_from_json_file('add_item.json')
-except Exception:
-    n = []
-
-n.extend(l)
-save_to_json_file(n, 'add_item.json')
+    try:
+        it = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        it = []
+    it.extend(sys.argv[1:])
+    save_to_json_file(it, "add_item.json")
