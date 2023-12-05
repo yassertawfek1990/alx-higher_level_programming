07d9c6f6880b2a579976fef3be9a0607c0b9b01b
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-05 14:14:21 -0800

    c

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define ffff"""
+
+
+def write_file(filename="", text=""):
+    """addsad add"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
