53f06f9f8b3b115ca73f81d3034569000702221b
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-05 15:30:07 -0800

    c

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """define dd"""
+
+
 import sys
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 l = list(sys.argv[1:])
+
 try:
     n = load_from_json_file('add_item.json')
 except Exception:
     n = []
+
 n.extend(l)
 save_to_json_file(n, 'add_item.json')
