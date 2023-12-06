312d31b51f0a96cba9b3012233fff3af756fda2b
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-05 16:50:25 -0800

    c

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""define dd"""
+
+
+class Student:
+    """Represeent."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary represe"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """nmsklsk sdsf"""
+        for x, b in json.items():
+            setattr(self, x, b)
