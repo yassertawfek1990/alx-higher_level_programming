4144b815a98360bc48f8e34373e9af980ce13622
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-05 16:36:25 -0800

    c

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+i#!/usr/bin/python3
+"""define dd"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Fcd Kdk"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dsdcds ds"""
+        if (type(attrs) == list and
+                all(type(e) == str for e in attrs)):
+            return {km: getattr(self, km) for km in attrs if hasattr(self, km)}
+        return self.__dict__
