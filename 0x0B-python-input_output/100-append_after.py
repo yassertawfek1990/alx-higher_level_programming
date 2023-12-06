cf551cb0f443ff76a076944b1145cd37ec32b913
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-05 16:57:43 -0800

    c

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,10 +5,12 @@
 def append_after(filename="", search_string="", new_string=""):
     """Insert te"""
     xt = ""
+
     with open(filename) as q:
         for l in q:
             xt += l
             if search_string in l:
                 xt += new_string
+
     with open(filename, "w") as z:
         z.write(xt)
