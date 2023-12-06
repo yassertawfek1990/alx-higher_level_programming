4625049d5eb1cdf809f26996fdceee324470bfc3
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-05 16:57:01 -0800

    c

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define dd"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert te"""
+    xt = ""
+    with open(filename) as q:
+        for l in q:
+            xt += l
+            if search_string in l:
+                xt += new_string
+    with open(filename, "w") as z:
+        z.write(xt)
