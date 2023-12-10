e3b05f79715cb2f586e1911980213a70ede6f0b3
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-10 12:48:02 -0800

    c

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """define dd"""
 import json
 
+
 class Base:
     """sfdsfsdf"""
 
@@ -13,9 +14,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    
+    @staticmethod
     def to_json_string(list_dictionaries):
         if not list_dictionaries or list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is not None:
+            list_objs = [x.to_dictionary() for x in list_objs]
+        with open("{}.json".format(cls.__name__), "w") as h:
+            h.write(cls.to_json_string(list_objs))
+
+
