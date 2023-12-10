a5a7a7d99aa4086b6b649918f3dbbfbdb371a1e8
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-10 12:06:54 -0800

    c

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,3 +10,30 @@ def Square(Rectangle):
      def __str__(self):
         """Re and str() rep"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.getter
+    def size(self, v):
+        self.width = v
+        self.height = v
+
+     def _update_(self,id=None,size=None,y=None,x=None):
+        """Update"""
+        if id != None:
+            self.id = id
+        if width != None:
+            self.size = size
+        if x != None:
+            self.x = x
+        if y != None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """asd fdf"""
+        if args:
+            self._update_(self, *args)
+        elif kwargs:
+            self._update_(self, **kwargs)
