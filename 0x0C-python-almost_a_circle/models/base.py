8f60febee433fb4a491fdb1a45692e3427d792ea
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-10 13:57:39 -0800

    c

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,5 +28,36 @@ class Base:
             list_objs = [x.to_dictionary() for x in list_objs]
         with open("{}.json".format(cls.__name__), "w") as h:
             h.write(cls.to_json_string(list_objs))
+    
+    @staticmethod
+    def from_json_string(json_string):
+        """sdv dvs"""
+        if not json_string or json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
 
+    @classmethod
+    def create(cls, **dictionary):
+        """fvdfv gfggd"""
+        from models.square import Square
+        from models.rectangle import Rectangle
+        if cls.__name__ is Square:
+            n = Squar(1)
+        elif cls.__name__ is Rectangle:
+            n = Rectangle(1, 1)
+        else:
+            n = None
+        n.update(**dictionary)
+        return n
+
+    @classmethod
+    def load_from_file(cls):
+        """sdf sdd"""
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(f, "r") as x:
+            return (cls.create(**m) for m in cls.from_json_string(x.read()))
 
