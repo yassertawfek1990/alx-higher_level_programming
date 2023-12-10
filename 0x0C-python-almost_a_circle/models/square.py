42f77da9173ce7202347648b448dc440ec5d3c02
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-10 06:56:41 -0800

    c

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define dd"""
+
+
+def Square(Rectangle):
+    """Square Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        Super().__init__(id)
+        Super().__init__(size)
+        Super().__init__(x)
+        Super().__init__(y)
