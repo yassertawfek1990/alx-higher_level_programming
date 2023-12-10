2dce514fe5bc1e2caec86a098430aca1b768aecb
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-10 11:48:45 -0800

    c

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,7 +5,8 @@
 def Square(Rectangle):
     """Square Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-        Super().__init__(id)
-        Super().__init__(size)
-        Super().__init__(x)
-        Super().__init__(y)
+        Super().__init__(size, size, x, y, id)
+
+     def __str__(self):
+        """Re and str() rep"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
