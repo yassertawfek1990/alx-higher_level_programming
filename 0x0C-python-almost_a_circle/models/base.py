4f09a812f64a7cb04a73d5fe72f6c3d04eafb92a
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-11 10:41:40 -0800

    c

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,93 +1,121 @@
 #!/usr/bin/python3
-"""define dd"""
-import json
+'''Moduse class.'''
+from json import dumps, loads
 import csv
 
 
 class Base:
-    """sfdsfsdf"""
+    '''A representatirarchy.'''
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''Cstuctor.'''
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
-        if not list_dictionaries or list_dictionaries is None:
+        '''Jsoes longer.'''
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
-            return json.dumps(list_dictionaries)
+            return dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Unjsdict ionary.'''
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        '''Savn ct tile.'''
         if list_objs is not None:
-            list_objs = [x.to_dictionary() for x in list_objs]
-        with open("{}.json".format(cls.__name__), "w") as h:
-            h.write(cls.to_json_string(list_objs))
-    
-    @staticmethod
-    def from_json_string(json_string):
-        """sdv dvs"""
-        if not json_string or json_string is None:
-            return []
-        else:
-            return json.loads(json_string)
+            list_objs = v.to_dictionary() for v in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as fo:
+            fo.write(cls.to_json_string(list_objs))
 
     @classmethod
     def create(cls, **dictionary):
-        """fvdfv gfggd"""
-        from models.square import Square
+        '''tance froionary.'''
         from models.rectangle import Rectangle
-        if cls.__name__ is Square:
-            n = Squar(1)
-        elif cls.__name__ is Rectangle:
+        from models.square import Square
+        if cls is Rectangle:
             n = Rectangle(1, 1)
+        elif cls is Square:
+            n = Square(1)
         else:
             n = None
-        n.update(**dictionary)
+        n.update(**dic)
         return n
 
     @classmethod
     def load_from_file(cls):
-        """sdf sdd"""
+        '''Loas fies.'''
         from os import path
         file = "{}.json".format(cls.__name__)
         if not path.isfile(file):
             return []
-        with open(f, "r") as x:
-            return (cls.create(**m) for m in cls.from_json_string(x.read()))
+        with open(file, "r", encoding="utf-8") as fo:
+            return [cls.create(**v) for v in cls.from_json_string(fo.read())]
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """vsdvsd sdvsv"""
-        from models.square import Square
+        '''Sav v file.'''
         from models.rectangle import Rectangle
+        from models.square import Square
         if list_objs is not None:
             if cls is Rectangle:
-                l = [q.id, q.width, q.height, q.x, q.y for q in list_objs]
+                l = [[v.id, v.width, v.height, v.x, v.y] for v in list_objs]
             else:
-                l = [q.id, q.size, q.x, q.y for q in list_objs]
-        with open("{}.csv".format(cls.__name__), "w",newline="") as fi:
-            w =csv.writer(fi)
+                l = [[v.id, v.size, v.x, v.y] for v in list_objs]
+        with open('{}.csv'.format(cls.__name__), 'w', newline='', encoding='utf-8') as fo:
+            w = csv.writer(fo)
             w.writerows(l)
+
     @classmethod
     def load_from_file_csv(cls):
-        """sdc sdvsdv"""
-        from models.square import Square
+        '''Loadsf ile.'''
         from models.rectangle import Rectangle
-        with open("{}.csv".format(cls.__name__), "r",newline="") as fi:
-            l = []
-            r = csv.reader(fi)
-            for s in r:
-                s =[int(x) for x in r]
+        from models.square import Square
+        w = []
+        with open('{}.csv'.format(cls.__name__), 'r', newline='', encoding='utf-8') as fo:
+            der = csv.reader(fo)
+            for r in der:
+                r = [int(h) for h in r]
                 if cls is Rectangle:
-                    k = ["id": r[0], "width": r[1], "height": r[2], "x": r[3], "y": r[4]]
+                    k = {"id": r[0], "width": r[1], "height": r[2], "x": r[3], "y": r[4]}
                 else:
-                     k = ["id": r[0], "size": r[1], "x": r[2], "y": r[3]]
-                l.append(cls.create(**k))
-        return l
+                    k = {"id": r[0], "size": r[1], "x": r[2], "y": r[3]}
+                w.append(cls.create(**k))
+        return w
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        import turtle
+        import time
+        from random import randrange
+        turtle.Screen().colormode(255)
+        for v in list_rectangles + list_squares:
+            t = turtle.Turtle()
+            t.color((randrange(255), randrange(255), randrange(255)))
+            t.pensize(1)
+            t.penup()
+            t.pendown()
+            t.setpos((v.x + t.pos()[0], v.y - t.pos()[1]))
+            t.pensize(10)
+            t.forward(v.width)
+            t.left(90)
+            t.forward(v.height)
+            t.left(90)
+            t.forward(v.width)
+            t.left(90)
+            t.forward(v.height)
+            t.left(90)
+            t.end_fill()
+        time.sleep(5)
