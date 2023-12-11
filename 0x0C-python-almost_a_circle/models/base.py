269ac789769ac0057b314c4d2e527e22ae60879c
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-11 12:32:22 -0800

    c

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,84 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Jsonifi longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Unj ictionary.'''
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Sav ile.'''
+        if list_objs is not None:
+            list_objs = [e.to_dictionary() for e in list_objs]
+        with open("{}.json".format(cls.__name__), "w") as fo:
+            fo.write(cls.to_json_string(list_objs))
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Load onary.'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            n = Rectangle(1, 1)
+        elif cls is Square:
+            n = Square(1)
+        else:
+            n = None
+        n.update(**dictionary)
+        return n
+
+    @classmethod
+    def load_from_file(cls):
+        '''Loa ffs.'''
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, "r") as fo:
+            return [cls.create(**t) for t in cls.from_json_string(fo.read())]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''Sav file.'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if list_objs is not None:
+            if cls is Rectangle:
+                list_objs = [[e.id, e.width, e.height, 
+                    e.x, e.y] for e in list_objs]
+            else:
+                list_objs = [[e.id, e.size, e.x, e.y] for e in list_objs]
+        with open('{}.csv'.format(cls.__name__), 'w', newline='') as fo:
+            w = csv.writer(fo)
+            w.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        '''Lo ile.'''
+        from models.rectangle import Rectangle
+        from models.square import Square
+        m = []
+        with open('{}.csv'.format(cls.__name__), 'r', newline='') as fo:
+            q = csv.reader(fo)
+            for r in q:
+                r = [int(c) for c in r]
+                if cls is Rectangle:
+                    k = {"id": r[0], "width": r[1], "height": r[2],
+                            "x": r[3], "y": r[4]}
+                else:
+                    k = {"id": r[0], "size": r[1], "x": r[2], "y": r[3]}
+                m.append(cls.create(**k))
+        return m
+
