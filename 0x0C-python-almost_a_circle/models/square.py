344c447d104759e6be3c891813b05dd62a2cfd5d
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2023-12-11 12:09:03 -0800

    c

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,25 @@ class Square(Rectangle):
     def size(self, b):
         self.width = b
         self.height = b
+
+    def __update__(self, id=None, size=None, x=None, y=None):
+        '''Int args.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''Updord asrgs.'''
+        if args:
+            self.__update__(*args)
+        elif kwargs:
+            self.__update__(**kwargs)
+
+    def to_dictionary(self):
+        '''Rens dic'''
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
