839c319136e3c5a7dffce3a27aa1823300a42025
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-12 23:23:43 +0200

    c

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const rg = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((c, v) => c - v);
+  console.log(rg[args.length - 2]);
+}
