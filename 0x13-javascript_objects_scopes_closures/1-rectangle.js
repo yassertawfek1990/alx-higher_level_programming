89bed414f455b8d7e5ff342b859147fbeb702602
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-13 22:49:38 +0200

    c

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    this.width = w;
+    this.height = h;
   }
-};
+}
+
+module.exports = Rectangle;
