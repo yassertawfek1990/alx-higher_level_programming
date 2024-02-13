0fdee3a731ca8c43dbdd5973749092436cd9ce77
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-13 20:50:57 +0200

    c

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,21 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
+
   print () {
-    for (let c = 0; c < this.height; c++) console.log('X'.repeat(this.width));
+    for (let c = 0; c < this.height; c++) {
+      let g = '';
+      for (let l = 0; l < this.width; l++) {
+        g += 'X';
+      }
+      console.log(s);
+    }
   }
-};
+}
+
+module.exports = Rectangle;
