4194f3e04a05eaac3a76731bf5690dc77c18e8b4
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-13 22:40:21 +0200

    c

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,15 +1,29 @@
-#!/usr/bin/node
-module.exports = class Rectangle {
+#!usr/bin/node
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
   print () {
-    for (let c = 0; c < this.height; c++) console.log('X'.repeat(this.width));
+    for (let d = 0; d < this.height; d++) {
+      let q = '';
+      for (let f = 0; f < this.width; f++) {
+        q += 'X';
+      }
+      console.log(q);
+    }
   }
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
   }
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    this.width *= 2;
+    this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
