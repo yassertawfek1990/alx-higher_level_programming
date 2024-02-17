4e3f830622426618fda70b4d58b8d331efc25184
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-17 21:54:33 +0200

    c

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" prinsthts """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    ge = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Sn = sessionmaker(bind=engine)
+    ss = Sn()
+    nce = ss.query(State).filter_by(id=2).first()
+    nce.name = 'New Mexico'
+    ss.commit()
