1ab72068904646db1336bcc154d33e58db7de2f4
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-17 22:41:37 +0200

    c

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     ge = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                        format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                              pool_pre_ping=True)
+                       pool_pre_ping=True)
     Base.metadata.create_all(ge)
     Sn = sessionmaker(bind=ge)
     ss = Sn()
