a22f31aaf104891b156b4132f168ac31ae8151a1
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-17 22:43:50 +0200

    c

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,8 +9,8 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     ge = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Sn = sessionmaker(bind=engine)
+    Base.metadata.create_all(ge)
+    Sn = sessionmaker(bind=ge)
     ss = Sn()
     nce = ss.query(State).filter_by(id=2).first()
     nce.name = 'New Mexico'
