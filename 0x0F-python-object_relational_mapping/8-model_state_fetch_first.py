08d5e46ee84472427db57bc68fdfa881b656cf01
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-17 21:36:40 +0200

    c

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     ge = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                      .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(ge)
     Sn = sessionmaker(bind=ge)
     ss = Sn()
