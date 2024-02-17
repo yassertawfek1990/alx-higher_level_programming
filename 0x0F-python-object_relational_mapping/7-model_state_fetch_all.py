6b2512090f07c9be55d279237d04f508b02099bc
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-17 21:30:48 +0200

    c

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Stardatabase
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    ge = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(ge)
+    Sn = sessionmaker(bind=ge)
+    ss = Sn()
+    for i in ss.query(State).order_by(State.id):
+        print(i.id, i.name, sep=": ")
