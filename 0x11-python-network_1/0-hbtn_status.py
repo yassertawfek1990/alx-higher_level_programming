c05450a2aa830635144ee8949ea0e2c8974955b6
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-03-01 18:49:46 +0200

    c

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    q urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    q = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(q) as s:
         d = s.read()
         print("Body response:")
