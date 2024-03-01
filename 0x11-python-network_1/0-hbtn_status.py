3a23dc7b1fbe4c4e078547e5513ad9f0e7a030ce
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-03-01 18:47:58 +0200

    c

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""F"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    q urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(q) as s:
+        d = s.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(d)))
+        print("\t- content: {}".format(d))
+        print("\t- utf8 content: {}".format(d.decode("utf-8")))
