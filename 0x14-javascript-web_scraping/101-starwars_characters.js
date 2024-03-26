1ac4d6fb75f17a0d84d6a6ec4d91ba70636a6b97
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-03-27 01:45:11 +0200

    c

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,21 +2,19 @@
 const x = require('request');
 const id = process.argv[2];
 const v = `https://swapi-api.alx-tools.com/api/films/${id}`;
-x.get(v, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const b = JSON.parse(body);
-    const z = b.characters;
-    for (const a of z) {
-      x.get(a, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const g = JSON.parse(body);
-          console.log(g.name);
-        }
-      });
-    }
+x(v, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+function printCharacters (characters, index) {
+  v(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
