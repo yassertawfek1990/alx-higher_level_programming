43828e275b4b5a7bf3eadb581c3fb41871133a4c
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-03-27 00:59:14 +0200

    c

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const s = require('request');
+const request = require('request');
 let r = 'http://swapi.co/api/films/' + process.argv[2];
-s(r, function (error, response, body) {
+request(r, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
